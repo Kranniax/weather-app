0db5710321fa1981becc4a09943c520c92ebe925
Shane Wade <shanebramble96@hotmail.com>
2023-07-11 19:11:53 -0500

    completed local storage for searched cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+var currentWeatherContainerEl = document.querySelector(
+  ".current-weather-container"
+);
+var fiveDayForecastContainerEl = document.querySelector(".five-day-container");
+var searchedCities = [];
+
 // Fetch geographic coordinates [lat, lon] by using name of location (city name or area name).
 var geoCoding = function (searchedCity) {
   fetch(
@@ -13,8 +19,6 @@ var geoCoding = function (searchedCity) {
 
 // Using the One Call API 3.0 search for the Citie's current and five day forcast.
 var citySearch = function (data) {
-  //  console.log(, data[0].lon);
-
   fetch(
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       data[0].lat +
@@ -23,14 +27,38 @@ var citySearch = function (data) {
       "&exclude={part}&units=imperial&appid=379d1420e9fea3af7ba71fd81914bc2f"
   ).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      currentWeather(data);
+      fiveDayForecast(data);
     });
   });
 };
+// showcase the current weather conditions.
+var currentWeather = function (data) {};
+// showcase the five day weather conditions. 
+var fiveDayForecast = function (data) {};
+// build the basic template for weather conditions. 
+var weatherTemplate = function (cityInput) {};
 
+var saveCity = function (city) {
+  if (localStorage.getItem("cities") === null) {
+    searchedCities.push(city);
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
+  } else {
+    searchedCities = JSON.parse(localStorage.getItem("cities"));
+    searchedCities.push(city);
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
+  }
+};
+
+// Extract the search city name from input form.
 var formHandler = function (e) {
   e.preventDefault();
   var cityInput = document.querySelector("#city-input").value.trim();
+  // store in localStorage.
+  saveCity(cityInput);
+  //weather template
+  weatherTemplate(cityInput);
+  // use fetch api to find weather details.
   geoCoding(cityInput);
 };
 
