c45b307ed06519c86e968d59471c724e82ac445b
Shane Wade <shanebramble96@hotmail.com>
2023-07-13 18:51:05 -0500

    create basic template for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,13 +12,13 @@ var geoCoding = function (searchedCity) {
       "&appid=379d1420e9fea3af7ba71fd81914bc2f"
   ).then(function (response) {
     response.json().then(function (data) {
-      citySearch(data);
+      citySearch(data, searchedCity);
     });
   });
 };
 
 // Using the One Call API 3.0 search for the Citie's current and five day forcast.
-var citySearch = function (data) {
+var citySearch = function (data, city) {
   fetch(
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       data[0].lat +
@@ -27,16 +27,40 @@ var citySearch = function (data) {
       "&exclude={part}&units=imperial&appid=379d1420e9fea3af7ba71fd81914bc2f"
   ).then(function (response) {
     response.json().then(function (data) {
-      currentWeather(data);
-      fiveDayForecast(data);
+      currentWeather(data, city);
+      // fiveDayForecast(data);
     });
   });
 };
 // showcase the current weather conditions.
-var currentWeather = function (data) {};
-// showcase the five day weather conditions. 
+var currentWeather = function (data, city) {
+  // clear old weather data first.
+  currentWeatherContainerEl.innerHTML = "";
+
+  // create current weather dom.
+  var cardContainerEl = document.createElement("div");
+  cardContainerEl.classList.add("card");
+  cardContainerEl.setAttribute("style", "width: 18rem");
+
+  var cardBodyEl = document.createElement("div");
+  cardBodyEl.classList.add("card-body");
+
+  var cardTitleEl = document.createElement("h5");
+  cardTitleEl.classList.add("card-title");
+  cardTitleEl.textContent = city;
+
+  var cardSubtitle = document.createElement("h6");
+  cardSubtitle.classList.add("card-subtitle", "mb-2", "text-body-secondary");
+  cardSubtitle.textContent = "Current Weather";
+
+  cardBodyEl.appendChild(cardTitleEl);
+  cardBodyEl.appendChild(cardSubtitle);
+  cardContainerEl.appendChild(cardBodyEl);
+  currentWeatherContainerEl.appendChild(cardContainerEl);
+};
+// showcase the five day weather conditions.
 var fiveDayForecast = function (data) {};
-// build the basic template for weather conditions. 
+// build the basic template for weather conditions.
 var weatherTemplate = function (cityInput) {};
 
 var saveCity = function (city) {
@@ -57,7 +81,7 @@ var formHandler = function (e) {
   // store in localStorage.
   saveCity(cityInput);
   //weather template
-  weatherTemplate(cityInput);
+  // weatherTemplate(cityInput);
   // use fetch api to find weather details.
   geoCoding(cityInput);
 };
